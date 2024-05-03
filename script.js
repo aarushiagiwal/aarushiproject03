198eb980f59d8e9a4bf2467d41918b9e403f1750
Leo Dominguez <adominguezleo@gmail.com>
2024-05-03 12:31:14 -0400

    it's almost there

--- script.js
+++ script.js
@@ -1,199 +1,201 @@
 const postcards = [
-    {
-        "imagefront": "front/image1.png",
-        "imageback": "back/image1.png",
-        "year": 1652,
-        "undefined": "wycoff"
-    },
-    {
-        "imagefront": "front/image2.png",
-        "imageback": "back/image2.png",
-        "year": 1661,
-        "undefined": "browne"
-    },
-    {
-        "imagefront": "front/image3.png",
-        "imageback": "back/image3.png",
-        "year": 1690,
-        "undefined": "alice austen"
-    },
-    {
-        "imagefront": "front/image4.png",
-        "imageback": "back/image4.png",
-        "year": 1690,
-        "undefined": "hisotric rismond town"
-    },
-    {
-        "imagefront": "front/image5.png",
-        "imageback": "back/image5.png",
-        "year": 1699,
-        "undefined": "old stone"
-    },
-    {
-        "imagefront": "front/image6.png",
-        "imageback": "back/image6.png",
-        "year": 1720,
-        "undefined": "hendrik l"
-    },
-    {
-        "imagefront": "front/image7.png",
-        "imageback": "back/image7.png",
-        "year": 1730,
-        "undefined": "king manor"
-    },
-    {
-        "imagefront": "front/image8.png",
-        "imageback": "back/image8.png",
-        "year": 1748,
-        "undefined": "vancortland"
-    },
-    {
-        "imagefront": "front/image9.png",
-        "imageback": "back/image9.png",
-        "year": 1758,
-        "undefined": "Valentine"
-    },
-    {
-        "imagefront": "front/image10.png",
-        "imageback": "back/image10.png",
-        "year": 1765,
-        "undefined": "morris jummel"
-    },
-    {
-        "imagefront": "front/image11.png",
-        "imageback": "back/image11.png",
-        "year": 1772,
-        "undefined": "queens country farm"
-    },
-    {
-        "imagefront": "front/image12.png",
-        "imageback": "back/image12.png",
-        "year": 1776,
-        "undefined": "conference house"
-    },
-    {
-        "imagefront": "front/image13.png",
-        "imageback": "back/image13.png",
-        "year": 1783,
-        "undefined": "lefferts"
-    },
-    {
-        "imagefront": "front/image14.png",
-        "imageback": "back/image14.png",
-        "year": 1784,
-        "undefined": "dykman"
-    },
-    {
-        "imagefront": "front/image15.png",
-        "imageback": "back/image15.png",
-        "year": 1785,
-        "undefined": "kingsland "
-    },
-    {
-        "imagefront": "front/image16.png",
-        "imageback": "back/image16.png",
-        "year": 1799,
-        "undefined": "gracie mantion"
-    },
-    {
-        "imagefront": "front/image17.png",
-        "imageback": "back/image17.png",
-        "year": 1812,
-        "undefined": "Edgar Allen"
-    },
-    {
-        "imagefront": "front/image18.png",
-        "imageback": "back/image18.png",
-        "year": 1832,
-        "undefined": "merchants house"
-    },
-    {
-        "imagefront": "front/image19.png",
-        "imageback": "back/image19.png",
-        "year": 1836,
-        "undefined": " Bartow-Pell Mansion Museum"
-    },
-    {
-        "imagefront": "front/image20.png",
-        "imageback": "back/image20.png",
-        "year": 1838,
-        "undefined": "seguine"
-    },
-    {
-        "imagefront": "front/image21.png",
-        "imageback": "back/image21.png",
-        "year": 1876,
-        "undefined": "swedish cottage"
-    },
-    {
-        "imagefront": "front/image22.png",
-        "imageback": "back/image22.png",
-        "year": 1887,
-        "undefined": "lewis latmir"
-    },
-    {
-        "imagefront": "front/image23.png",
-        "imageback": "back/image23.png",
-        "year": 1921,
-        "undefined": "littleredlight"
-    }
-]
+  {
+    imagefront: "front/image1.png",
+    imageback: "back/image1.png",
+    year: 1652,
+    undefined: "wycoff",
+  },
+  {
+    imagefront: "front/image2.png",
+    imageback: "back/image2.png",
+    year: 1661,
+    undefined: "browne",
+  },
+  {
+    imagefront: "front/image3.png",
+    imageback: "back/image3.png",
+    year: 1690,
+    undefined: "alice austen",
+  },
+  {
+    imagefront: "front/image4.png",
+    imageback: "back/image4.png",
+    year: 1690,
+    undefined: "hisotric rismond town",
+  },
+  {
+    imagefront: "front/image5.png",
+    imageback: "back/image5.png",
+    year: 1699,
+    undefined: "old stone",
+  },
+  {
+    imagefront: "front/image6.png",
+    imageback: "back/image6.png",
+    year: 1720,
+    undefined: "hendrik l",
+  },
+  {
+    imagefront: "front/image7.png",
+    imageback: "back/image7.png",
+    year: 1730,
+    undefined: "king manor",
+  },
+  {
+    imagefront: "front/image8.png",
+    imageback: "back/image8.png",
+    year: 1748,
+    undefined: "vancortland",
+  },
+  {
+    imagefront: "front/image9.png",
+    imageback: "back/image9.png",
+    year: 1758,
+    undefined: "Valentine",
+  },
+  {
+    imagefront: "front/image10.png",
+    imageback: "back/image10.png",
+    year: 1765,
+    undefined: "morris jummel",
+  },
+  {
+    imagefront: "front/image11.png",
+    imageback: "back/image11.png",
+    year: 1772,
+    undefined: "queens country farm",
+  },
+  {
+    imagefront: "front/image12.png",
+    imageback: "back/image12.png",
+    year: 1776,
+    undefined: "conference house",
+  },
+  {
+    imagefront: "front/image13.png",
+    imageback: "back/image13.png",
+    year: 1783,
+    undefined: "lefferts",
+  },
+  {
+    imagefront: "front/image14.png",
+    imageback: "back/image14.png",
+    year: 1784,
+    undefined: "dykman",
+  },
+  {
+    imagefront: "front/image15.png",
+    imageback: "back/image15.png",
+    year: 1785,
+    undefined: "kingsland ",
+  },
+  {
+    imagefront: "front/image16.png",
+    imageback: "back/image16.png",
+    year: 1799,
+    undefined: "gracie mantion",
+  },
+  {
+    imagefront: "front/image17.png",
+    imageback: "back/image17.png",
+    year: 1812,
+    undefined: "Edgar Allen",
+  },
+  {
+    imagefront: "front/image18.png",
+    imageback: "back/image18.png",
+    year: 1832,
+    undefined: "merchants house",
+  },
+  {
+    imagefront: "front/image19.png",
+    imageback: "back/image19.png",
+    year: 1836,
+    undefined: " Bartow-Pell Mansion Museum",
+  },
+  {
+    imagefront: "front/image20.png",
+    imageback: "back/image20.png",
+    year: 1838,
+    undefined: "seguine",
+  },
+  {
+    imagefront: "front/image21.png",
+    imageback: "back/image21.png",
+    year: 1876,
+    undefined: "swedish cottage",
+  },
+  {
+    imagefront: "front/image22.png",
+    imageback: "back/image22.png",
+    year: 1887,
+    undefined: "lewis latmir",
+  },
+  {
+    imagefront: "front/image23.png",
+    imageback: "back/image23.png",
+    year: 1921,
+    undefined: "littleredlight",
+  },
+];
 
-window.addEventListener("load",()=>{
-    const initialEnvelope = document.getElementById('initial-envelope')
-    initialEnvelope.addEventListener("click", (e)=>{
-        displayData(postcards);
-    })
+window.addEventListener("load", () => {
+  const initialEnvelope = document.getElementById("initial-envelope");
+  initialEnvelope.addEventListener("click", (e) => {
+    displayData(postcards);
+  });
+});
 
-})
+// This is looping over the post cards a second time
+// unecessarily. All of your looping code should be inside
+// your displayData function inside the forEach loop that's already set up.
 
-for (let postcard of postcards){
-    const itemElement = document.createElement("div");
-    itemElement.addEventListener("click", ()=>{
-        activatePopUp(postcard);
-    })
-    itemElement.innerHTML = buildPostcardListItem(postcard);
-
-    container.appendChild(itemElement);
-    }
-
-    const activatePopUp = function(postcard){
+for (let postcard of postcards) {
 
 
-        console.log(postcard);
-        document.getElementById("output-container").classList += 'activate';
-        var popUp = document.getElementById("popup");
-        popUp.style.display = "block";
-        var popUpDetails = document.getElementById("pop-up-details");
-     popUpDetails.innerHTML = buildPostcardDetailItem(postcard);
-        
-    }
+  // This chunk should all be happening in the forEach loop in your displayData 
+  // function instead of the one line that's in there now.
+  const itemElement = document.createElement("div");
+  itemElement.addEventListener("click", () => {
+    activatePopUp(postcard);
+  });
+  itemElement.innerHTML = buildPostcardListItem(postcard);
 
-function displayData(postcards){
-    var container = document.getElementById('output-container'); 
-    
-    
+  container.appendChild(itemElement);
 
-    const buildPostcardListItem = function(postcard){
-        return `<div class="container">
+
+}
+
+function displayData(postcards) {
+  var container = document.getElementById("output-container");
+
+  const activatePopUp = function (postcard) {
+    console.log(postcard);
+    document.getElementById("output-container").classList += "activate";
+    var popUp = document.getElementById("popup");
+    popUp.style.display = "block";
+    var popUpDetails = document.getElementById("pop-up-details");
+    popUpDetails.innerHTML = buildPostcardDetailItem(postcard);
+  };
+
+  const buildPostcardListItem = function (postcard) {
+    return `<div class="container">
         <img class="image" src = '${postcard.imagefront}'/>  
         <p class="year">${postcard.year}<p/>  
         </div>`;
-    }
+  };
 
-    const buildPostcardDetailItem = function(postcard){
-        return `<div class="detail-view">
+  const buildPostcardDetailItem = function (postcard) {
+    return `<div class="detail-view">
         <img class="detailimg" src = '${postcard.imagefront}'/> 
     
         </div>`;
-    }
+  };
 
-    postcards.forEach((postcard)=>{
-        container.innerHTML += buildPostcardListItem(postcard)
-    })
+  postcards.forEach((postcard) => {
+    container.innerHTML += buildPostcardListItem(postcard);
+  });
 
-
-    const initialEnvelope = document.getElementById('initial-envelope')
-initialEnvelope.style.display = "none"
+  const initialEnvelope = document.getElementById("initial-envelope");
+  initialEnvelope.style.display = "none";
 }
-
